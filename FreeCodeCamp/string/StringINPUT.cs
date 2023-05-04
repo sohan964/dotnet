8ad32b820889a7edb067a8e35de7672953603769
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-05-04 23:34:52 +0600

    String INPUT way

---
+++ FreeCodeCamp/string/StringINPUT.cs
@@ -0,0 +1,23 @@
+// See https://aka.ms/new-console-template for more information
+
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+
+namespace firstProgram{
+    class Program{
+        static void Main(string[] args){
+            
+            Console.Write("Enter your Name: ");
+            string name = Console.ReadLine();//input string
+            Console.WriteLine(name);
+            
+            
+
+            Console.ReadLine();//pos the program to enter something
+        }
+    }
+}
