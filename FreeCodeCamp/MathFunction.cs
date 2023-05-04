c544e506b34ebea96166ac965fbee9d94c04b1f5
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-05-04 22:42:31 +0600

    some Math function

---
+++ FreeCodeCamp/MathFunction.cs
@@ -0,0 +1,31 @@
+// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstProgram{
+    class Program{
+        static void Main(string[] args){
+
+            int a=6;
+            a--;
+            Console.WriteLine(a);
+            Console.WriteLine(Math.Pow(2,2)); //4
+            Console.WriteLine(Math.Sqrt(9)); //3
+            Console.WriteLine(Math.Abs(-10));  //10
+            Console.WriteLine(Math.Max(4,5)); //5
+            Console.WriteLine(Math.Min(7,8));//7
+            Console.WriteLine(Math.Round(10.6));//10
+            Console.WriteLine(Math.Ceiling(5.4)); //6
+            Console.WriteLine(Math.Log2(10)); //
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
