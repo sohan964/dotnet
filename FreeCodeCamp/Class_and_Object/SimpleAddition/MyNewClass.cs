30a7ce1c6d34575425d937971230d4826ffbe054
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-05-07 18:27:17 +0600

    This class is connect with the Main Function

---
+++ FreeCodeCamp/Class_and_Object/SimpleAddition/MyNewClass.cs
@@ -0,0 +1,14 @@
+// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+
+
+namespace firstProgram{
+    class MyNewClass{
+        public int Addition(int a, int b){
+           return a+b;
+        }
+    }
+}
