464bd2d4df051368cc6b4c44cc1e99e00e08420e
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-05-12 20:30:59 +0600

    here is the Main method

---
+++ BangarRaju/Constractor/AccessSpecifier/fiveTypes_Specifier/Accesscon.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Raju{
+    class Accesscon : Program{
+          
+        static void Main(){
+            //Program f1 = new Program();
+            Accesscon f2 = new Accesscon();
+            f2.Test2();
+            f2.Test4();
+            f2.Test5();
+            f2.Test3();
+            
+        }
+    }
+}
