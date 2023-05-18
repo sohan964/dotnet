d78b3e5366ec326c7ddc8bdf1cba3d5abc2ffcab
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-05-18 23:16:43 +0600

    using (new) to possible  re-emplement

---
+++ BangarRaju/Method_Hidding|Showding/Hidding/loadP.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raju{
+    public class loadP{
+        public void Test(){
+            Console.WriteLine("Parent's Method");
+        }
+    }
+    class loadC: loadP{
+        public new void Test(){ // Method hiding/shadowing
+            Console.WriteLine("Child's Method");
+        }
+        public void Ptest(){
+            //without creating the costractor of loadP class
+            base.Test(); //it will called loadP Test Method
+        }
+        
+        static void Main(){
+            loadC c1 = new loadC();
+            c1.Test();
+            c1.Ptest();
+            
+
+        }
+    }
+    
+}
