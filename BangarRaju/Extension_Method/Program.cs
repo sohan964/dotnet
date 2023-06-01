c99c3b8c587a4a56ddd70807893f05768b3e9520
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-06-01 12:23:50 +0600

    Test3_Method add on this class

---
+++ BangarRaju/Extension_Method/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    internal class Program
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Test1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Test2");
+
+        }
+        
+    }
+}
