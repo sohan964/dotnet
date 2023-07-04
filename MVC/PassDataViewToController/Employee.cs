a909baf433ca2109b3780877c1b0e4f90f87f02d
Sohanur Rahman <86511149+sohan964@users.noreply.github.com>
2023-07-04 10:29:25 +0600

    this model need for passdata usingbinding

---
+++ MVC/PassDataViewToController/Employee.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassDataViewToController.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
